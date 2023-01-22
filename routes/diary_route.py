5fc07d79f8a290805fb2ac459528f51f5c1295a9
sean-park6353 <package9@naver.com>
2023-01-22 23:51:41 +0900

    어느정도 완성..?

--- routes/diary_route.py
+++ routes/diary_route.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
 
-diary_router = APIRouter(prefix="/diary", tags="일기")
+router = APIRouter(prefix="/diary", tags=["일기라우터"])
 
+@router.get("", description="일기장 기본 페이지",)
+async def get_diary():
+    return "2322"
