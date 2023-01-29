5145108d5853239794884bbb407fa6c88be13ebd
sean-park6353 <package9@naver.com>
2023-01-29 11:15:39 +0900

    도커파일 변경해서 로그 확인:

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ WORKDIR /app/
 COPY . /app/
 COPY ./requirements.txt /app/
 
-RUN pip install -r requirements.txt && apt-get update && apt-get install -y vim && apt-get install -y zsh
-RUN sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-CMD uvicorn --host=0.0.0.0 --port 8000 main:app
+RUN pip install -r requirements.txt && export MYSQL_URL=mysql+aiomysql://admin:o815o511@database-2.cwwalfzheezp.ap-northeast-2.rds.amazonaws.com:3306/jiseong2?charset=UTF8MB4
+
+CMD uvicorn --host=127.0.0.1 --port 80 main:app
--- routes/diary_route.py
+++ routes/diary_route.py
@@ -2,7 +2,11 @@ from fastapi import APIRouter, Depends
 from async_database import get_session, AsyncSession
 from sqlalchemy import select, insert
 from models.model import Diary, User
+import logging
 
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(filename="./server.log", level=logging.INFO)
 router = APIRouter(prefix="/diary", tags=["일기라우터"])
 
 @router.get("", description="전체 일기 페이지",)
@@ -10,7 +14,17 @@ async def get_diary(
     user_name: str,
     session: AsyncSession = Depends(get_session)
 ):
-    query = select(User).where(User.name==user_name)
-    coroutine_result = await session.execute(query)
-    result = coroutine_result.scalars().all()
-    return result
+    try:
+        query = select(User).where(User.name==user_name)
+        coroutine_result = await session.execute(query)
+        result = coroutine_result.scalars().all()
+        logger.info("!"*30)
+        logger.info(e)
+        logger.info("!"*30)
+        return result
+
+    except Exception as e:
+        logger.info("!"*30)
+        logger.info(e)
+        logger.info("!"*30)
+        
