eb9ed346b12f6ce546ab2b3e19e4ebd107a7d320
sean-park6353 <package9@naver.com>
2023-01-29 13:56:53 +0900

    ff

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 from routes import diary_route
-import os
 app = FastAPI()
 
-print("*"*40)
-print(os.getenv("MYSQL_URL"))
-print("*"*40)
+
 app.include_router(diary_route.router)
 
 @app.get("/", description="나무 페이지", tags=["API"])
